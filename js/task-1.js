6dee271b6fd7d034e436144070e78b370524bd8d
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-03-23 11:22:34 +0200

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,16 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-const name = 'Галина';
-const age = 35;
-const skill = 'JAVASCRIPT';
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, мені ${age} років, я вивчаю ${skill}`;
-console.log(message.toLowerCase());
+// const name = 'Галина';
+// const age = 35;
+// const skill = 'JAVASCRIPT';
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, мені ${age} років, я вивчаю ${skill}`;
+// console.log(message.toLowerCase());
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const email = prompt("enter your email");
+// const name = prompt("enter your name");
+// const tel = prompt("enter your tel");
+// const message = ` Користувач ${name}, використовує ${email}, та ${tel}. `;
+// console.log(message);
