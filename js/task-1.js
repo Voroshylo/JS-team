e34c960c3fe9d50b4307441e47d0ce9c0688a296
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-03-23 10:10:16 +0200

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+const name = 'Галина';
+const age = 35;
+const skill = 'JAVASCRIPT';
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, мені ${age} років, я вивчаю ${skill}`;
+console.log(message.toLowerCase());
