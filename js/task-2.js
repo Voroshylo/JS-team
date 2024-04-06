ecfb0074121256023bc9d00e986c4f9eda1fe5df
kittswaye <153619900+kittswaye@users.noreply.github.com>
2024-04-06 11:44:46 +0300

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -24,16 +24,49 @@
 // console.log(styles);
 
 // ЗАДАЧА 2
-// використовуючи цикл for , який для кожного елемента масива буде виводити в консоль 
+// використовуючи цикл for , який для кожного елемента масива буде виводити в консоль
 // повідомлення у форматі <індекс елемента> та <значення елемента>.
 // Номерація для кожного елемента масива ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено 1 - Mango, а для индекса 2 виведе 3 - Ajax.
 
-function indexElement () {
-    let indexes = ['Mango', 'Poly', 'Ajax'];
+// function indexElement () {
+//     let indexes = ['Mango', 'Poly', 'Ajax'];
 
-    for(const index of indexes) {
-        console.log(`${indexes.indexOf(index)} - ${index}`);
+//     for(const index of indexes) {
+//         console.log(`${indexes.indexOf(index)} - ${index}`);
+//     }
+// }
+
+// indexElement();
+
+// ЗАДАЧА 3
+// Напишіть код , який шукає саме найменше число в масиві
+// 1) напишіть циклом for
+// const numbers = [2, 17, 94, 1, 23, 37];
+// 2) перепишіть функцією findSmallestNumber(numbers)
+// console.log(findSmallestNumber([2, 17, 94, 1, 23, 37]));
+// console.log(findSmallestNumber([49, 4, 83, 7, 12]));
+
+// const numbers = [17, 2, 94, 1, 23, 37];
+// let smallestNumber = numbers[0];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (smallestNumber > numbers[i]) {
+//         smallestNumber = numbers[i];
+//     }
+// }
+
+// console.log(smallestNumber);
+
+function findSmallestNumber(numbers) {
+    let smallestNumber = numbers[0];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (smallestNumber > numbers[i]) {
+        smallestNumber = numbers[i];
     }
 }
+    return smallestNumber;
+}
 
-indexElement();
+console.log(findSmallestNumber([2, 17, 94, 1, 23, 37]));
+console.log(findSmallestNumber([49, 4, 83, 7, 12]));
